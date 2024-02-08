b899af75831749aafc2890fd8114f64054dbe31a
Isabel Rico <marissa.rico28@gmail.com>
2024-02-08 20:20:31 +0100

    fixing buttons

--- js/colours.js
+++ js/colours.js
@@ -6,3 +6,21 @@ function saveButton() {
 document.getElementById("saveButton").addEventListener("click", function (e) {
   saveButton();
 });
+
+//picking colors
+window.addEventListener("load", () => startup());
+
+let colorPicker = document.getElementsByClassName("colorPicker");
+let arrayColorPicker = Array.from(colorPicker);
+let objectChosenColours = {};
+let arrayChosenColours = [];
+
+// FUNCTION called startup-gatter colour pickers starts any color -same  array
+
+const startup = () => {
+  arrayColorPicker.map((element) => {
+    element.value = "#FFFF00";
+    element.addEventListener("input", (event) => updateSquare(event, element));
+    element.select();
+  });
+};
